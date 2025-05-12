dc0ba17f09852742f01e65972ec041a7f007b648
Soham Nandi <soham943325@gmail.com>
2025-05-12 01:20:20 -0700

    Completed the evetns and other sections

--- src/components/Gallery.jsx
+++ src/components/Gallery.jsx
@@ -1,4 +1,29 @@
 import React from 'react'
+import {useState} from 'react';
+import {useRef} from 'react';
+const BentoTilt=({children, className=''})=>{
+  const [transformStyle, setTransformStyle] = useState('');
+  const itemRef = useRef();
+  const handleMouseMove=(e)=>{
+    if(!itemRef.current) return;
+    const {left, top, width, height} = itemRef.current.getBoundingClientRect();
+    const relativeX =(e.clientX - left)/width;
+    const relativeY=(e.clientY - top)/height;
+    const tiltX=(relativeY - 0.5)*10;
+    const tiltY=(relativeX - 0.5)*-10;
+    const newTransform=`perspective(700px) rotateX(${tiltX}deg) rotateY(${tiltY}deg) scale3d(0.95,0.95,0.95)`;
+
+    setTransformStyle(newTransform);
+  }
+  const handleMouseLeave=()=>{
+    setTransformStyle('');
+  }
+  return(
+    <div className={className} ref={itemRef} onMouseMove={handleMouseMove} onMouseLeave={handleMouseLeave} style={{transform: transformStyle}} >
+      {children}  
+    </div>
+  )
+}
 
 const BentoCard = ({src, title,description})=>{
   return(
@@ -41,12 +66,39 @@ const Gallery = () => {
         </p>
       </div>
       
-      <div className='border-hsla relative mb-7 h-96 w-full overflow-hidden rounded-md md:h-[65vh]'>
+      <BentoTilt className='border-hsla relative mb-7 h-96 w-full overflow-hidden rounded-md md:h-[65vh]'>
         <BentoCard
-        src="videos/feature-1.mp4"
+        src="videos/featueju-2.mp4"
         title={<> Gall<b>e</b>ry</>}
         description="Explore the world of Power Engineering through our curated gallery, showcasing cutting-edge technologies and innovations."
         />
+
+      </BentoTilt>
+      <div className='grid h-[135vh] grid-cols-2 grid-rows-3 gap-7'>
+        <BentoTilt className='bento-tilt_1 row-span-1 md:col-span-1 md:row-span-2'>
+          <BentoCard
+          src="videos/featureju-1.mp4"
+          title={<>Eve<b>n</b>ts</>}
+          description="Join us in our journey of innovation and excellence. Stay updated with our latest events and initiatives."
+          />
+
+        </BentoTilt>
+        <BentoTilt className='bento-tilt_1 row-span-1  ms-32 md:col-span-1 md:ms-0'>
+          <BentoCard
+          src="videos/featureju-3.mp4"
+          title={<>Alu<b>m</b>ni</>}
+          description="Connect with our vibrant alumni community. Share experiences, insights, and opportunities in the field of Power Engineering."
+          />
+
+        </BentoTilt>
+        <BentoTilt className='bento-tilt_1 row-span-1  ms-32 md:col-span-1 md:ms-0'>
+          <BentoCard
+          src="videos/featureju-4.mp4"
+          title={<>MileSto<b>n</b>es</>}
+          description="Celebrate our achievements and milestones in the field of Power Engineering. Together, we are shaping the future."
+          />
+
+        </BentoTilt>
 
       </div>
     </div>
